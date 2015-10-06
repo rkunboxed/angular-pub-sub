b282469d268ad4b89b8fee514236668cba2b9821
Owen Buckley <obuckley@kenzan.com>
2015-10-06 12:07:09 -0400

    refactored NotificationsDirective to support callbacks

--- components/notifications/notifications-factory.js
+++ components/notifications/notifications-factory.js
@@ -1,14 +1,23 @@
 'use strict';
 
+/*
+ * The NotificationsFactory handles all the websocket communication and callbacks
+ * And provides methods for getting various data about notifications (notificications data, count, hasNotifications)
+ *
+ * The Factory can also support registering callbacks for (in this example) push based / socket events
+ */
 (function (angular) {
 
   angular
     .module('notifications')
     .factory('NotificationsFactory', notificationsFactory);
 
-  function notificationsFactory() {
+  //$interval is only here to mock a push based protoocal, like websockets
+  notificationsFactory.$inject = ['$interval'];
 
-    //simulating an API response
+  function notificationsFactory($interval) {
+
+    //simulating an API response or websocket callbacks
     var mockNotifications = [{
       title: 'Notification 1'
     },{
@@ -25,12 +34,25 @@
 
     var NotificationsFactory = function(){
       var notifications = [];
+      var callbacks = [];
+
+      function publishEvent(){
+        angular.forEach(callbacks, function(obj, idx){
+          console.log('publish event for ' + obj.name);
+          obj.func(notifications);
+        });
+      }
+
+      $interval(function(){
+        var rand = Math.floor((Math.random() * mockNotifications.length) + 1);
+
+        notifications = mockNotifications.slice(0, rand);
+
+        publishEvent();
+      }, 3000);
 
       return {
         getNotifications: function(){
-          var rand = Math.floor((Math.random() * mockNotifications.length) + 1);
-
-          notifications = mockNotifications.slice(0, rand);
 
           return notifications;
         },
@@ -39,9 +61,15 @@
           return notifications.length;
         },
 
-
         hasNotifications: function () {
           return notifications.length > 0;
+        },
+
+        registerCallback: function(callbackName, callbackFunction){
+          callbacks.push({
+            name: callbackName,
+            func: callbackFunction
+          })
         }
 
       };
